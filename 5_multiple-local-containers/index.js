fee1b0c1e013f29a13bc7093e9a207f5b6d61d51
Eduardo Aguilar <tallitosan@gmail.com>
2023-08-29 22:33:29 -0600

    add couple of notes and also done with the first docker-compose file

--- 5_multiple-local-containers/index.js
+++ 5_multiple-local-containers/index.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const redis = require('redis')
-
+const process = require('process')
 const app = express()
+
 const client = redis.createClient({
-  host: 'redis-server',
-  port: 6379
+  host: 'redis-server', // container name
+  port: 6379 // redis default port
 })
+
 client.set('visits', 0)
 
 app.get('/', (req, res) => {
+  process.exit(0) // to make server crash on purpose
   client.get('visits', (err, visits) => {
-    res.send('Number of visits is ' + visits)
+    res.send(`Number of visits is ${visits}`)
     client.set('visits', parseInt(visits) + 1)
   })
 })
 
-app.listen(8081, console.log(`Running on http://localhost:${8081}`))
+const PORT = 8081
+app.listen(PORT, console.log(`Running on http://localhost:${PORT}`))
+// the port in dockerfile is different from this, actually we can see the server in http://localhost:4000
